d8817828e6ee34b04525162be2756d86498fd586
Svante Bengtson <svante.b37@gmail.com>
2020-09-20 11:23:57 +0000

    implement challenge based oper

--- src/challenge.js
+++ src/challenge.js
@@ -1,27 +1,41 @@
+import { readFileSync } from 'fs'
+import path from 'path'
+import { createPrivateKey, privateDecrypt, createHash } from 'crypto'
+
 export default function ChallengeMiddleware () {
+  let challenge, key
+
   return function constructor (client, rawEvents, parsedEvents) {
     rawEvents.use(ratboxRespond)
-    parsedEvents.use(startChallenge)
 
     client.on('connected', event => {
-      const raw = client.rawString('CHALLENGE', process.env.CHALLENGE_USERNAME)
-      client.raw(raw)
+      startChallenge(client)
     })
   }
 
-  function startChallenge (command, event, client, next) {
-    next()
+  function startChallenge (client) {
+    challenge = ''
+
+    const keyfile = readFileSync(path.join(process.cwd(), path.normalize(process.env.CHALLENGE_PATH)), { encoding: 'utf-8' })
+    key = createPrivateKey({ key: keyfile, passphrase: process.env.CHALLENGE_PASS })
+
+    client.raw(`CHALLENGE ${process.env.CHALLENGE_USERNAME}`)
   }
 
   function ratboxRespond (command, event, raw, client, next) {
-    // console.log(command, event.params)
     if (command === '740') { // RPL_RSACHALLENGE2
-      console.log(event.params)
-      // this.challenge += event.params[1]
+      challenge += event.params[1]
     } else if (command === '741') { // RPL_ENDOFRSACHALLENGE2
+      // reset first to prevent garbage in the buffer on error
+      const ct = Buffer.from(challenge, 'base64')
+      challenge = ''
 
+      const decryptedChallenge = privateDecrypt(key, ct)
+      const hash = createHash('sha1').update(decryptedChallenge).digest('base64')
+
+      client.raw(`CHALLENGE :+${hash}`)
     } else if (command === '491') {
-      throw Error('Cannot Op')
+      throw Error(raw)
     }
 
     next()
